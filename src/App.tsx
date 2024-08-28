28c75a35da4909efd088371bba6efd25f80722c0
goncalveshigino <goncalveshigino20@gmail.com>
2024-08-28 12:56:12 +0100

    feat: Adiconado tema Light e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
+
+import { LightTheme } from './shared/themes';
 import { AppRoutes } from './routes';
+
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
+   <ThemeProvider theme={LightTheme}>
+     <BrowserRouter>
+       <AppRoutes />
+     </BrowserRouter>
+   </ThemeProvider>
   );
 }
 
