9a39ea50501940568df2b2d62e7e27f6fffff3e6
goncalveshigino <goncalveshigino20@gmail.com>
2024-08-29 16:35:32 +0100

    feat:Barra de ferramentas de listagem parte 2

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,9 +1,18 @@
+import { BarraDeFerramentas } from "../../shared/components";
 import { LayoutBasePagina } from "../../shared/layouts";
 
 export const Dashboard = () => {
 
     return (
-        <LayoutBasePagina titulo="Pagina inicial" barraDeFerramentas={<>Barra de Ferramentas</>}>
+        <LayoutBasePagina 
+          titulo="Pagina inicial" 
+          barraDeFerramentas={(
+            <BarraDeFerramentas 
+                mostrarInputBusca
+                //textoBotaoNovo="Nova"
+            />
+          )}
+        >
             Testando
         </LayoutBasePagina>
     )
