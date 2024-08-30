e4d7d0479b82ad96da087e5093153a9f441085ba
goncalveshigino <goncalveshigino20@gmail.com>
2024-08-30 14:58:32 +0100

    feat: Adiciona variaveis de ambientes

--- src/shared/environment/index.ts
+++ src/shared/environment/index.ts
@@ -1,1 +1,16 @@
-export {}
+
+
+export const Environment = {
+/**
+ * Define a quantidade de linhas a ser carregada por padrão nas listagens
+ */
+    LIMIT_DE_LINHAS: 10,
+/**
+ * Placeholder exibido nas inputs
+ */
+    INPUT_DE_BUSCA: 'Pesquisar...', 
+
+    LISTAGEM_VAZIA: 'Nenhum Registro encontrado.',
+    
+    URL_BASE: 'http://localhost:3333'
+}
